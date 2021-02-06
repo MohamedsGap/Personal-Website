a358079e4e76d25a8a94a87425cb52a16157a23c
mohamedsgap <mohdabdellnasser1@gmail.com>
2021-02-06 16:11:42 +0000

    Deploy website - based on cde34fb63adbcca40924f031b328a6160f7a0c87

---
+++ 6d886a3c.6818ef06.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{47:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return i}));var r=e(0),a=e.n(r),c=e(82);function i(n){var t=n.imgURL,e=Object(c.a)(t);return a.a.createElement("img",{src:e,alt:"card-img"})}}}]);
